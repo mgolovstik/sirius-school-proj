bd25a459ba3213adfea4a635520d05a834d30adc
necroshine0 <oepetrov0@gmail.com>
2025-12-24 12:41:37 +0300

    service processing

---
+++ service/app/model_call.py
@@ -0,0 +1,15 @@
+import mlflow
+import pandas as pd
+from dotenv import load_dotenv
+
+assert load_dotenv()
+
+
+def to_pandas(raw_json):
+	...
+	return df_point
+
+def predict_ad(raw_json):
+	df_point = to_pandas(raw_json)
+
+	
